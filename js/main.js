712aaa57885dc389a07e215ec547f0b994082ee8
Ryle <itsryle.urcia@gmail.com>
2020-10-08 14:41:08 -0400

    Added code in JS file for sliders in the testimonials section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+// Slider
+const slyderArray = document.querySelectorAll('.testimonial-cards');
+const buttons = document.querySelectorAll('.slyde__arrow');
+let active = 0; //Will hold active card in  Array
 
+// Add Event Listeners to Buttons
+
+// Left Arrow
+buttons[0].addEventListener('click', getPrev);
+// Right Arrow
+buttons[1].addEventListener('click', getNext);
+
+// Functions for event listener call backs
+
+// FixMe: When active gets to 5, it should reset back to 0
+function getNext() {
+	// toggle inactive for current slide
+	slyderArray[active].classList.toggle('active');
+	slyderArray[active].classList.toggle('inactive');
+	active++;
+	// toggle active for next slide
+	slyderArray[active].classList.toggle('active');
+	slyderArray[active].classList.toggle('inactive');
+
+	console.log(active);
+}
+
+function getPrev() {
+	// toggle inactive for current slide
+	slyderArray[active].classList.toggle('active');
+	slyderArray[active].classList.toggle('inactive');
+	active--;
+	// toggle active for next slide
+	slyderArray[active].classList.toggle('active');
+	slyderArray[active].classList.toggle('inactive');
+
+	console.log(active);
+}
